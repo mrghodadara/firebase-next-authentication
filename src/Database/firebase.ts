b81236760d1162ae7cb83af194f49ddd43419645
mrghodadara <vgghodadara2003@gmail.com>
2025-05-07 18:34:08 +0530

    Env added and firebase var use from env

--- src/Database/firebase.ts
+++ src/Database/firebase.ts
@@ -8,12 +8,13 @@ import { getFirestore } from 'firebase/firestore';
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: 'AIzaSyAGF6kk_oUIlRGntb7Crg1T8TDVUaaxmVA',
-  authDomain: 'fir-next-authentication.firebaseapp.com',
-  projectId: 'fir-next-authentication',
-  storageBucket: 'fir-next-authentication.appspot.com',
-  messagingSenderId: '636454307303',
-  appId: '1:636454307303:web:03fa1fbaa0a762c517a942',
+  apiKey: process?.env?.NEXT_PUBLIC_FIREBASE_API_KEY || '',
+  authDomain: process?.env?.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN || '',
+  projectId: process?.env?.NEXT_PUBLIC_FIREBASE_PROJECT_ID || '',
+  storageBucket: process?.env?.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET || '',
+  messagingSenderId:
+    process?.env?.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID || '',
+  appId: process?.env?.NEXT_PUBLIC_FIREBASE_APP_ID || '',
 };
 
 // Initialize Firebase
